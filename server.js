ffde33f16b1d3be3b01804d5da156f19cb91fc88
philb56 <phil.bugden@outlook.com>
2018-03-07 13:45:46 +0000

    Add first routes to server.js Add app.listen to server.js

--- server.js
+++ server.js
@@ -6,4 +6,38 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const User = require('./models/user.js')
 const app = express();
-const url = 'pbugden:123@ds259268.mlab.com:59268/users';
+const url = process.env.MONGOLAB_URI;
+//====ROOT DIRECTORY===//
+app.get('/', function(req, res) {
+  res.json('you did it');
+});
+//==========================//
+//====GET ALL SIGNATURES===//
+app.get('/api/users', function(req, res) {
+  User.find({}).then(eachOne => {
+    res.json(eachOne);
+    })
+  })
+//==========================//
+//====POST NEW SIGNATURE===//
+app.post('/api/users', function(req, res) {
+  User.create({
+    name: req.body.Name,
+    skills: req.body.Skills,
+    email: req.body.Email,
+  }).then(user => {
+    res.json(user)
+  });
+});
+
+//====MONGOOSE CONNECT===//
+mongoose.connect(url, function (err, db) {
+ if (err) {
+   console.log('Unable to connect to the mongoDB server. Error:', err);
+ } else {
+   console.log('Connection established to', url);
+ }
+});
+//==========================//
+app.listen(process.env.PORT || 3000);
+console.log('starting applicaiton.  Good job!');
